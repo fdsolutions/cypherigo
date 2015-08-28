46d04c68c2e69cbb8dcdb3f6f915bdb06534444d
first-developer <lioninho11@hotmail.fr>
2015-08-28 09:17:50 +0200

    Introduce a new Node DSL with Ident, WithLabels funcs

--- pttrns.go
+++ pttrns.go
@@ -2,25 +2,45 @@ package cypherigo
 
 import (
 	"bytes"
-	///"github.com/hashicorp/go-multierror"
+	"fmt"
+	"github.com/lann/builder"
+	"strings"
 )
 
-type pttrn interface {
-	Node(name string) Cypherizer
+type prop map[string]interface{}
+
+type nodeData struct {
+	Ident      string
+	WithLabels []string
+	WithProps  []prop
 }
 
-type node struct {
-	name string
+// NodeBuilder builds Cypher Node
+type nodeBuilder builder.Builder
+
+var Node = builder.Register(nodeBuilder{}, nodeData{}).(nodeBuilder)
+
+func (d nodeData) ToCypher() (string, error) {
+	fmt.Printf("%#v", d)
+	lbls := d.WithLabels
+	l := len(d.WithLabels)
+	symbols := make([]string, l)
+	for _, lbl := range lbls {
+		sym := fmt.Sprintf(":%s", bytes.Title([]byte(lbl)))
+		symbols = append(symbols, sym)
+	}
+	return fmt.Sprintf("(%s%s)", d.Ident, strings.Join(symbols, "")), nil
 }
 
-func (n *node) ToCypher() (string, error) {
-	cypher := &bytes.Buffer{}
-	cypher.WriteString(tokName(openParenthesis))
-	cypher.WriteString(n.name)
-	cypher.WriteString(tokName(closeParenthesis))
-	return cypher.String(), nil
+func (b nodeBuilder) Ident(name string) nodeBuilder {
+	return builder.Set(b, "Ident", name).(nodeBuilder)
 }
 
-func Node(name string) Cypherizer {
-	return &node{name}
+func (b nodeBuilder) WithLabels(lbls ...string) nodeBuilder {
+	return builder.Set(b, "WithLabels", lbls).(nodeBuilder)
+}
+
+func (b nodeBuilder) ToCypher() (string, error) {
+	d := builder.GetStruct(b).(nodeData)
+	return d.ToCypher()
 }
--- pttrns_test.go
+++ pttrns_test.go
@@ -9,10 +9,22 @@ import (
 
 var _ = Describe("Pttrns", func() {
 	Describe("Node", func() {
-		Context("Node(a)", func() {
-			It("returns (a)", func() {
-				n := Node(`a`)
+		Context("Node", func() {
+			It("returns: ()", func() {
+				n := Node
+				Expect(n.ToCypher()).To(Equal(`()`))
+			})
+		})
+		Context("Node.Ident(`a`)", func() {
+			It("returns: (a)", func() {
+				n := Node.Ident("a")
 				Expect(n.ToCypher()).To(Equal(`(a)`))
+			})
+		})
+		Context("Node.Ident(`a`).WithLabels(`Person`, `Swedish`)", func() {
+			It("returns: (a:Person:Swedish)", func() {
+				n := Node.Ident("a").WithLabels("Person", "Swedish")
+				Expect(n.ToCypher()).To(Equal(`(a:Person:Swedish)`))
 			})
 		})
 	})
